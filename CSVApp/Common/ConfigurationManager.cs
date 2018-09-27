b554f75f475a2a2ff734e0ff307f3b8034f1778f
juliazhan <771450210@qq.com>
2018-09-27 16:21:33 +0800

    study .net core console

---
+++ CSVApp/Common/ConfigurationManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSVApp.Common
+{
+    public class ConfigurationManager
+    {
+        private static IConfigurationRoot config = null;
+        private static string _defaultPath = Directory.GetCurrentDirectory()+"\\app.json" ;
+        //private static string rootdir = AppContext.BaseDirectory;
+        //private static DirectoryInfo Dir = Directory.GetParent(rootdir);
+        //private static string root = Dir.Parent.Parent.Parent.FullName+"\\app.json";
+        static ConfigurationManager()
+        {
+            // Microsoft.Extensions.Configuration扩展包提供的
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile(_defaultPath);
+            config = builder.Build();
+        }
+
+        public static IConfigurationRoot AppSettings
+        {
+            get
+            {
+                return config;
+            }
+        }
+
+        public static string Get(string key)
+        {
+            return config[key];
+        }
+    }
+}
---
+++ CSVApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using CSVApp.Common;
+using CsvHelper;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace CSVApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine("start as exe");                
+            } 
+            else
+            {
+                Console.WriteLine("start as service");
+            }
+            Thread thread = new Thread(myFirstThread);
+            thread.Start();           
+        }
+        static void myFirstThread()
+        {
+            Console.WriteLine("Hello world");
+            Log.i("this is a test");
+            Thread.Sleep(3000);
+        }
+    }
+}
